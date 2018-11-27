d77ed781f2e030cb9a7c98aeb7085060b1f220c4
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2018-11-27 18:48:49 +1000

    fix: refactor as class
    BREAKING CHANGE: requires upgrade of ut-port and ut-run

--- index.js
+++ index.js
@@ -1,64 +1,48 @@
 'use strict';
-const merge = require('lodash.merge');
-const util = require('util');
 
-module.exports = function({parent}) {
-    function ScriptPort({config}) {
-        parent && parent.apply(this, arguments);
-        this.config = merge({
-            id: null,
-            logLevel: 'info',
-            type: 'script',
-            findMethod: false
-        }, config);
+function findMethod(where, methodName) {
+    let result = where[methodName];
+    if (!result) {
+        let names = methodName.split('.');
+        while (names.length) {
+            result = where[names.join('.')];
+            if (result) {
+                where[methodName] = result;
+                break;
+            }
+            names.pop();
+        }
     }
+    return result;
+}
 
-    if (parent) {
-        util.inherits(ScriptPort, parent);
-    }
-
-    function findMethod(where, methodName) {
-        let result = where[methodName];
-        if (!result) {
-            let names = methodName.split('.');
-            while (names.length) {
-                result = where[names.join('.')];
-                if (result) {
-                    where[methodName] = result;
-                    break;
-                }
-                names.pop();
+module.exports = function({parent, utPort = parent}) {
+    return class ScriptPort extends utPort {
+        get defaults() {
+            return {
+                type: 'script',
+                findMethod: false
+            };
+        }
+        async exec(...params) {
+            let $meta = params && params.length > 1 && params[params.length - 1];
+            let methodName = ($meta && $meta.method) || 'exec';
+            let method = this.config.findMethod ? findMethod(this.methods, methodName) : this.methods[methodName];
+            if (!method) {
+                methodName = methodName.split('/', 2);
+                method = (methodName.length === 2 && this.methods[methodName[1]]) || this.methods.exec;
+            }
+            if (method instanceof Function) {
+                return method(...params);
+            } else {
+                throw this.bus.errors['bus.methodNotFound']({ params: { method: $meta && $meta.method } });
             }
         }
-        return result;
-    }
-
-    ScriptPort.prototype.exec = function(...params) {
-        let $meta = params && params.length > 1 && params[params.length - 1];
-        let methodName = ($meta && $meta.method) || 'exec';
-        let method = this.config.findMethod ? findMethod(this.config, methodName) : this.config[methodName];
-
-        if (!method) {
-            methodName = methodName.split('/', 2);
-            method = (methodName.length === 2 && this.config[methodName[1]]) || this.config.exec;
-        }
-
-        if (method instanceof Function) {
-            return Promise.resolve().then(() => method.apply(this, params));
-        } else {
-            return Promise.reject(this.bus.errors['bus.methodNotFound']({params: {method: $meta && $meta.method}}));
+        async start() {
+            this.bus.importMethods(this.methods, this.config.imports, { request: true, response: true }, this);
+            const result = await super.start(...arguments);
+            this.pull(this.exec, this.config.context);
+            return result;
         }
     };
-
-    ScriptPort.prototype.start = function(...params) {
-        this.bus.importMethods(this.config, this.config.imports, {request: true, response: true}, this);
-        return Promise.resolve()
-            .then(() => parent && parent.prototype.start.apply(this, params))
-            .then(result => {
-                this.pull(this.exec, this.config.context);
-                return result;
-            });
-    };
-
-    return ScriptPort;
 };
